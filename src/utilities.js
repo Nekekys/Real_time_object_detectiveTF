8d9792e346e1df9136263b88ec9ec39fbaa09cb5
Nekekys <geri_337@mail.ru>
2021-08-21 12:35:12 +0300

    first commit

---
+++ src/utilities.js
@@ -0,0 +1,17 @@
+export const drawRect = (detections, ctx) =>{
+  detections.forEach(prediction => {
+
+    const [x, y, width, height] = prediction['bbox']; 
+    const text = prediction['class']; 
+
+    const color = 'red';
+    ctx.strokeStyle = color
+    ctx.font = '18px Arial';
+
+    ctx.beginPath();   
+    ctx.fillStyle = color
+    ctx.fillText(text, x, y - 7);
+    ctx.rect(x, y, width, height); 
+    ctx.stroke();
+  });
+}
